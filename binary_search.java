f0f746f9aeae362eadcadd255c3a4b15e99a29bf
Raghav Bajaj <73180455+Raghav-Bajaj@users.noreply.github.com>
2023-04-11 20:43:16 +0530

    Update binary_search.java

--- binary_search.java
+++ binary_search.java
@@ -33,6 +33,8 @@ class BinarySearch {
 		BinarySearch ob = new BinarySearch();
 		int arr[] = { 2, 3, 4, 10, 40 };
 		int n = arr.length;
+		
+	}}
 		int x = 10;
 		int result = ob.binarySearch(arr, 0, n - 1, x);
 		if (result == -1)
